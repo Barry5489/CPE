c2d73f5f656550858733cd66baf18bcb0548bb6e
Barry5489 <fowjoy107080@gmail.com>
2025-06-10 21:30:37 +0800

    Initial commit

---
+++ Desktop/Cproject/FCUOJ/fcuoj homework/final exam/week 14/class practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class TicTacToe{
+    private:
+        vector <vector <int>> vec;
+    public:
+        TicTacToe() : vec(3, vector<int>(3, 0)) {}
+};
+int main(){
+
+}
---
+++ Desktop/Cproject/FCUOJ/fcuoj homework/final exam/week 14/project2/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "myheader.h"
+using namespace std;
+int main(){
+    show();
+}
