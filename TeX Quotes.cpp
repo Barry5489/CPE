ab11cf3174319777c0ec82bb7f6be6ba30794075
Barry5489 <fowjoy107080@gmail.com>
2025-07-25 16:47:01 +0800

    first commit

---
+++ TeX Quotes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string str1,str2;
+    bool DDD=true;
+    while(getline(cin,str1)){
+        str2 += str1+"\n";
+        size_t pos = str2.find("\"");
+        while(pos != std::string::npos){
+            if(DDD){
+                str2.replace(pos,1,"``");
+                DDD = false;
+            }else{
+                str2.replace(pos,1,"''");
+                DDD = true;
+            }
+            pos = str2.find("\"",+2);
+        }
+        cout<<str2<<endl;
+    }
+}
